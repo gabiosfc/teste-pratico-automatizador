5bdc96d2625f54ffabd2d8be8b0e0aa0a188ddff
Gabriela Correia <95930520+gabiosfc@users.noreply.github.com>
2023-07-18 01:05:28 -0300

    Update SeleniumAutomatizadorTest.java

--- teste3/src/test/java/br/com/nttdata/teste3/SeleniumAutomatizadorTest.java
+++ teste3/src/test/java/br/com/nttdata/teste3/SeleniumAutomatizadorTest.java
@@ -6,39 +6,51 @@ import static org.junit.jupiter.api.Assertions.*;
 public class SeleniumAutomatizadorTest {
 
     @Test
-    public void teste_success(){
-        Massas massas = SeleniumAutomatizador.cenario1();
-        Massas massaDB = getMassasFromDB();
-        assertEquals(massaDB.getCUSTOMIZATION(), massas.getCUSTOMIZATION());
-        assertEquals(massaDB.getDISPLAY(), massas.getDISPLAY());
-        assertEquals(massaDB.getDISPLAY_RESOLUTION(), massas.getDISPLAY_RESOLUTION());
-        assertEquals(massaDB.getDISPLAY_SIZE(), massas.getDISPLAY_SIZE());
-        assertEquals(massaDB.getMEMORY(), massas.getMEMORY());
-        assertEquals(massaDB.getOPERATING_SYSTEM(), massas.getOPERATING_SYSTEM());
-        assertEquals(massaDB.getPROCESSOR(), massas.getPROCESSOR());
-        assertEquals(massaDB.getTOUCHSCREEN(), massas.getTOUCHSCREEN());
-        assertEquals(massaDB.getWEIGHT(), massas.getWEIGHT());
+    public void teste_cenario1_success(){
+
+        SeleniumAutomatizador seleniumAutomatizador = new SeleniumAutomatizador();
+
+        Massas massaResult = seleniumAutomatizador.cenario1();
+        Massas massaDB = seleniumAutomatizador.getMassasDB();
+
+        assertEquals(massaDB.getCUSTOMIZATION(), massaResult.getCUSTOMIZATION());
+        assertEquals(massaDB.getDISPLAY(), massaResult.getDISPLAY());
+        assertEquals(massaDB.getDISPLAY_RESOLUTION(), massaResult.getDISPLAY_RESOLUTION());
+        assertEquals(massaDB.getDISPLAY_SIZE(), massaResult.getDISPLAY_SIZE());
+        assertEquals(massaDB.getMEMORY(), massaResult.getMEMORY());
+        assertEquals(massaDB.getOPERATING_SYSTEM(), massaResult.getOPERATING_SYSTEM());
+        assertEquals(massaDB.getPROCESSOR(), massaResult.getPROCESSOR());
+        assertEquals(massaDB.getTOUCHSCREEN(), massaResult.getTOUCHSCREEN());
+        assertEquals(massaDB.getWEIGHT(), massaResult.getWEIGHT());
+    }
+
+    @Test
+    public void teste_cenario2_success() {
+        SeleniumAutomatizador seleniumAutomatizador = new SeleniumAutomatizador();
+
+        String result = seleniumAutomatizador.cenario2();
+        assertEquals(seleniumAutomatizador.getMassasDB().getCOLOR(), result);
+    }
+
+    @Test
+    public void teste_cenario3_success() {
+        SeleniumAutomatizador seleniumAutomatizador = new SeleniumAutomatizador();
+
+        assertEquals("GRAY", seleniumAutomatizador.getMassasDB().getCOLOR());
+
+        boolean result = seleniumAutomatizador.cenario3();
+        assertTrue(result);
+
+        assertEquals("BLACK", seleniumAutomatizador.getMassasDB().getCOLOR());
 
     }
 
-    //Mockando busca de massa no BD:
-    //insert into massas(NAME_PRODUCT,CUSTOMIZATION,DISPLAY,DISPLAY_RESOLUTION,DISPLAY_SIZE,MEMORY,OPERATING_SYSTEM,PROCESSOR,TOUCHSCREEN,WEIGHT,COLOR) values("HP PAVILION 15Z TOUCH LAPTOP","Simplicity","15.6-inch diagonal Full HD WLED-backlit Display (1920x1080) Touchscreen","1920x1080","15.6","16GB DDR3 - 2 DIMM","Windows 10","AMD Quad-Core A10-8700P Processor + AMD Radeon(TM) R6 Graphics","Yes","5.51 lb","GRAY");
-    private Massas getMassasFromDB() {
-        Massas massa = new Massas();
-
-        massa.setNAME_PRODUCT("HP PAVILION 15Z TOUCH LAPTOP");
-        massa.setCUSTOMIZATION("Simplicity");
-        massa.setDISPLAY("15.6-inch diagonal Full HD WLED-backlit Display (1920x1080) Touchscreen");
-        massa.setDISPLAY_RESOLUTION("1920x1080");
-        massa.setDISPLAY_SIZE("15.6");
-        massa.setMEMORY("16GB DDR3 - 2 DIMM");
-        massa.setOPERATING_SYSTEM("Windows 10");
-        massa.setPROCESSOR("AMD Quad-Core A10-8700P Processor + AMD Radeon(TM) R6 Graphics");
-        massa.setTOUCHSCREEN("Yes");
-        massa.setWEIGHT("5.51 lb");
-        massa.setCOLOR("GRAY");
-
-        return massa;
-
+    @Test
+    public void teste_cenario4_success() {
+        SeleniumAutomatizador seleniumAutomatizador = new SeleniumAutomatizador();
+        boolean result = seleniumAutomatizador.cenario4();
+        assertTrue(result);
     }
+
+
 }
